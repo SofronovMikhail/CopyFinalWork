0bb17a05ea701ff54b1951c0f6a640f35b1c5552
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-03-11 00:02:27 +0300

    added a method get sorting array

--- Project/Program.cs
+++ Project/Program.cs
@@ -22,3 +22,23 @@ int Sort(string[] getArray1)
    return count;
 }
 int quantity = Sort(getArray);
+
+
+string[] SortArray(string[] getArray2, int quantity1)
+{
+    int j = 0;
+    string[] sortArray = new string[quantity1];
+    for(int i = 0; i < getArray2.Length; i++)
+    { 
+        if(getArray2[i].Length <= 3)
+        {
+            sortArray[j] = getArray2[i];
+            j++;
+        }
+    }
+    return sortArray;
+}
+string[] sortArray = SortArray(getArray, quantity);
+
+Console.WriteLine($"[{String.Join(", ", getArray)}]");
+Console.WriteLine($"[{String.Join(", ", sortArray)}]");
