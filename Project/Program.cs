dc71e67d60dac5e1dfad5db4abd5fe330e96af6d
Sofronov Mikhail <MishaSof2008@yandex.ru>
2023-03-10 23:53:00 +0300

    method string get array

---
+++ Project/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите елементы массива через пробел: ");
+string element = Console.ReadLine();
+
+string[] GetArray(string el)
+{
+    string[] num = el.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    return num;
+}
+string[] getArray = GetArray(element);
